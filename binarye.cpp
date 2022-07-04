b1b02d20ee6b8e46f8c45845d0563449d732c590
ariclavel <a01706629@itesm.mx>
2022-07-04 12:28:28 -0400

    a

--- binarye.cpp
+++ binarye.cpp
@@ -8,14 +8,17 @@
 #include <cmath>
 #include <set>
 using namespace std;
-void binarySearch(int a){
-
+void binarySearch(int a, int b, int c){
+    int p1 = 0;
+    int p2 = 0;
 }
 int main(){
-    int n,temp; 
+    int n,a,b,c; 
     cin>>n;
     for(int i = 0; i < n; i++){
-        cin>>temp;
-        binarySearch(temp);
+        cin>>a;
+        cin>>b;
+        cin>>c;
+        binarySearch(a,b,c);
     }
 }
