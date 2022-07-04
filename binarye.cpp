7262f816bb4de19b8a6a926782baf86f0019f59b
ariclavel <a01706629@itesm.mx>
2022-07-04 12:37:18 -0400

    cambios

--- binarye.cpp
+++ binarye.cpp
@@ -7,13 +7,22 @@
 #include <iostream>
 #include <cmath>
 #include <set>
+#include <iomanip>
 using namespace std;
-void binarySearch(int a, int b, int c){
-    int p1 = 0;
-    int p2 = 0;
+void binarySearch(float a, float b, float c){
+    float p1 = 1;
+    float p2 = c;
+    float mid;
+    while(p1 != p2){
+        mid = p1+(p2-p1)/2;
+        if(a*mid+b*sin(mid)==c) cout<<setprecision(1000)<<mid;
+        if(a*mid+b*sin(mid)>c) p2 = mid-1;
+        else p1 = mid+1;
+    }
+
 }
 int main(){
-    int n,a,b,c; 
+    float n,a,b,c; 
     cin>>n;
     for(int i = 0; i < n; i++){
         cin>>a;
