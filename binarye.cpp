f98ae1d827ffd6b729f6e5cc33d641eb830be0e3
ariclavel <a01706629@itesm.mx>
2022-07-04 13:22:41 -0400

    cambios

--- binarye.cpp
+++ binarye.cpp
@@ -9,20 +9,29 @@
 #include <set>
 #include <iomanip>
 using namespace std;
-void binarySearch(float a, float b, float c){
-    float p1 = 1;
-    float p2 = c;
-    float mid;
+void binarySearch(double a, double b, double c){
+    double p1 = 1;
+    double p2 = c;
+    double mid;
     while(p1 != p2){
         mid = p1+(p2-p1)/2;
-        if(a*mid+b*sin(mid)==c) cout<<setprecision(1000)<<mid;
-        if(a*mid+b*sin(mid)>c) p2 = mid-1;
-        else p1 = mid+1;
+        cout<<mid<<endl;
+        /*double xDegrees = 90.0;
+  
+        // converting degrees to radians
+        mid = xDegrees*3.14159/180;*/
+        if(a*mid+b*sin(mid)<c && a*(mid+0.00001)+b*sin(mid+0.00001)>c){
+            cout<<"igual"<<endl;
+            cout<<mid;
+            return;
+        } 
+        if(a*mid+b*sin(mid)>c) p2 = mid-0.00001;
+        else p1 = mid+0.00001;
     }
 
 }
 int main(){
-    float n,a,b,c; 
+    double n,a,b,c; 
     cin>>n;
     for(int i = 0; i < n; i++){
         cin>>a;
